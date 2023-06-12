5baa0462fd8e039ddf55bdd27420b53a8bf975ac
chinedu moses <chinedu2moses@gmail.com>
2023-06-12 01:09:53 -0500

    css modification

---
+++ assets/js/question.js
@@ -0,0 +1,70 @@
+
+const quizQuestions = [
+    {
+        question: "What is my full name?",
+        choices: ["Chinedu Moses", "Chindu Moes", "cimens Mitisco", "Magnu nwagi"],
+        correctAnswer: "Chinedu Moses"
+    },
+
+    {
+        question: "What is my fevorite sports?",
+        choices: ["Soccer", "Baseball", "Football", "swimming"],
+        correctAnswer: "Soccer"
+    },
+
+    {
+        question: "What city do you live at?",
+        choices: ["Nashville", "Atlanta", "chicago", "Cincinati"],
+        correctAnswer: "Nashville"
+    },
+
+    {
+        question: "What is my full name?",
+        choices: ["Chinedu Moses", "Chindu Moes", "cimens Mitisco", "Magnu nwagi"],
+        correctAnswer: "Chinedu Moses"
+    },
+
+    {
+        question: "What is my fevorite sports?",
+        choices: ["Soccer", "Baseball", "Football", "swimming"],
+        correctAnswer: "Soccer"
+    },
+
+    {
+        question: "What city do you live at?",
+        choices: ["Nashville", "Atlanta", "chicago", "Cincinati"],
+        correctAnswer: "Nashville"
+    },
+    
+    {
+        question: "What is my full name?",
+        choices: ["Chinedu Moses", "Chindu Moes", "cimens Mitisco", "Magnu nwagi"],
+        correctAnswer: "Chinedu Moses"
+    },
+
+    {
+        question: "What is my fevorite sports?",
+        choices: ["Soccer", "Baseball", "Football", "swimming"],
+        correctAnswer: "Soccer"
+    },
+
+    {
+        question: "What city do you live at?",
+        choices: ["Nashville", "Atlanta", "chicago", "Cincinati"],
+        correctAnswer: "Nashville"
+    },
+];
+
+const funImages = [
+    './assets/images/happy fun.gif',
+    './assets/images/hellYeah.gif',
+    './assets/images/winning.gif',
+    './assets/images/yeah.gif',
+    './assets/images/you did it.gif' 
+];
+
+const sadImages = [
+    './assets/images/damn.gif',
+    './assets/images/oops.gif',
+    './assets/images/pretty bad.gif',
+]
